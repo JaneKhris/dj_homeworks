ee9b6ffd83c104abe2a982752cfc51cf04562e4f
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-01-13 13:15:36 +0300

    homework3 task1

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +9,26 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
+    param_sort= request.GET.get('sort','no_sort')
     context = {}
+    if param_sort == 'no_sort':
+        phones = Phone.objects.all()
+    elif param_sort == 'name':
+        phones = Phone.objects.order_by('name')
+    elif param_sort == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif param_sort == 'max_price':
+        phones = Phone.objects.order_by('-price')
+
+
+    print(param_sort)
+    context['phones'] = phones
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     context = {}
+    phone = get_object_or_404(Phone,slug = slug)
+    context['phone'] = phone
     return render(request, template, context)

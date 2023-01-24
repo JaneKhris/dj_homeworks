019105d53cc59c0496bf5f6476b708c59d95af16
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-01-24 14:34:04 +0300

    homework database2 task2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        k = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main')==True:
+                k += 1
+        if k != 1:
+            raise ValidationError('Ошибка')
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at']
+    inlines = [ScopeInline,]
+
+@admin.register(Tag)
+class TegAdmin(admin.ModelAdmin):
+    list_display = ['name',]

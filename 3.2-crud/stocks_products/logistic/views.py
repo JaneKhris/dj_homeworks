c0814e1495027ba65676c7a2230aa6a840119ed5
Evgeniya Khrsanfova <efkhrisanfova@gmail.com>
2023-05-20 01:16:53 +0300

    homework_crud

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,3 +1,4 @@
+import django_filters
 from rest_framework.viewsets import ModelViewSet
 
 from logistic.models import Product, Stock
@@ -7,10 +8,17 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    search_fields = ['title','description']
 
+class StockFilter(django_filters.FilterSet):
+    products=django_filters.CharFilter(field_name='positions__product')
+
+    class Meta:
+        model = Stock
+        fields = ['products']
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filterset_class = StockFilter
+    search_fields = ['positions__product__title','positions__product__description']
